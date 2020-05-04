5a544bc74911fa67efb768e952e93ccde9b16e84
jingxin wang <wjxalexander@hotmail.com>
2020-05-04 18:44:11 +0800

    badstack

--- graphsearch/week1/stronglyconnectedcomponent.js
+++ graphsearch/week1/stronglyconnectedcomponent.js
@@ -1,99 +1,143 @@
-const fs = require("fs")
-const R = require("ramda")
+const fs = require("fs");
+const R = require("ramda");
+const dayjs = require("dayjs");
+var localizedFormat = require("dayjs/plugin/localizedFormat");
+dayjs.extend(localizedFormat);
 
-fs.readFile("./graphsearch/week1/scc.txt",'utf-8',(error,data)=>{
-    const adjList = data.toString().split("\n").map(item=>item && item.trim())
-    excution(adjList)
-})
+let adjListArray = [];
 
-// console.log(adjListArray)
-class Graph{
-    constructor(){
-        this.adjectList = new Map()
+// 这道题用递归容易爆栈 只能用迭代的方式来模拟递归
+
+fs.readFile("./graphsearch/week1/scc.txt", "utf-8", (err, data) => {
+  adjListArray = data
+    .toString()
+    .split("\n")
+    .map(item => item.trim());
+
+  excute(adjListArray);
+});
+
+class Graph {
+  constructor(pros) {
+    this.adjList = new Map();
+    // this.maxVertex = 0
+  }
+  addVertex = v => {
+    this.adjList.set(v, []);
+  };
+  addEdge = (source, target) => {
+    if (!this.adjList.has(source)) {
+      this.addVertex(source);
     }
-    addVerTex = (v)=>{
-        this.adjectList.set(v, [])
+    if (!this.adjList.has(target)) {
+      this.addVertex(target);
     }
-    addEdge = (source, target) =>{
-        if(!this.adjectList.has(source)){
-            this.addVerTex(source)
-        }
-        if(!this.adjectList.has(target)){
-            this.addVerTex(target)
-        }
-        this.adjectList.get(source).push(target)
-    }
+    this.adjList.get(source).push(target); // direct graph
+  };
 }
 
-function excution(list){
-    const originGraph = new Graph()
-    const reverseGraph = new Graph()
-    // build graph
-    list.forEach(item=>{
-        const [source,target] = item.split(" ")
-        originGraph.addEdge(source,target)
-        reverseGraph.addEdge(target,source)
-    })
+function excute(adjListArray) {
+  // 创建图 反向图
+  const originGraph = new Graph();
+  const reverseGraph = new Graph();
+  console.log(dayjs().format("L LT"));
+  adjListArray.forEach(item => {
+    const [source, target] = item.split(" ");
+    const intSource = parseInt(source);
+    const intTarget = parseInt(target);
+    originGraph.addEdge(intSource, intTarget);
+    reverseGraph.addEdge(intTarget, intSource);
+  });
+  console.log(dayjs().format("L LT"));
+  const largestKey = reverseGraph.adjList.size;
 
-    // topoSort Grev
-    const explored = new Set()
-    const finishTimeArray = []
-    // const topoSortRet = []
-    let globalTime = 0
-    reverseGraph.adjectList.forEach((val,key,map)=>{
-        if(!explored.has(key)){
-            explored.add(key)
-            const tempStack = [key]
-            while(!R.isEmpty(tempStack)){
-                const topEle = tempStack.pop()
-                const values = reverseGraph.adjectList.get(topEle)
-                values.forEach(item=>{
-                    if(!explored.has(item)){
-                        explored.add(item)
-                        tempStack.push(item)
-                    }
-                })
-                finishTimeArray.unshift(top)
-                globalTime++
-                // topoSortRet.push([top,globalTime])
-            }
+  const magicExplored = new Set();
+  const finishTime = new Map();
+  let count = 1; //finishtime
+  const keyStack = [];
+
+  // topo sort in arbitary order
+  for (let [key] of reverseGraph.adjList) {
+    if (!magicExplored.has(key)) {
+      dfs_magic_help(reverseGraph.adjList, key);
+    }
+  }
+  function dfs_magic_help(graph, vertex) {
+    magicExplored.add(vertex);
+    const tempStack = [vertex];
+    while (tempStack.length > 0) {
+      const key = tempStack.pop();
+      const verticesTodo = graph.get(key);
+      // magicExplored.add(key)
+      if (
+        R.isEmpty(verticesTodo) ||
+        R.all(item => magicExplored.has(item), verticesTodo)
+      ) {
+        finishTime.set(count, key);
+        keyStack.push(key);
+        count++;
+        continue;
+      }
+      for (let i = 0; i < verticesTodo.length; i++) {
+        const item = verticesTodo[i];
+        if (!magicExplored.has(item)) {
+          magicExplored.add(item);
+          tempStack.push(item);
         }
-    })
-    //SCC-DFS LOOP begin with the sink group from toposort
-    let numScc = 0;
-    const sccCnt = []
-    // const sccs =[]
-    // const ret = []
-    let explored_1 = new Set()
-    finishTimeArray.forEach(item=>{
-        if(!explored_1.has(item)){
-            const tempStack = [item]
-            explored_1.add(item)
-            numScc++
-            sccCnt[numScc] = 1
-            // const tempRet = [item]
-            while(!R.isEmpty(temStack)){
-                const top = tempStack.pop()
-                const values = originGraph.adjectList.get(top)
-                values.forEach(key=>{
-                    if(!explored_1.has(key)){
-                        explored_1.add(key);
-                        sccCnt[numScc]++;
-                        tempStack.push(key)
-                        // sccs[key] = numScc
-                        // tempRet.push(key)
-                    }
-                })
-            }
-            // ret.push(tempRet)
+      }
+    }
+  }
+  // not work stack overflow
+  async function dfs_magic_recursive_help(graph, vertex) {
+    magicExplored.add(vertex);
+    const verticesToDFS = graph.get(vertex);
+    for (let i = 0; i < verticesToDFS.length; i++) {
+      if (magicExplored.has(verticesToDFS[i])) {
+        await dfs_magic_recursive_help(graph, verticesToDFS[i]);
+      }
+    }
+    finishTime.set(count, vertex);
+    count++;
+  }
+  console.log(dayjs().format("L LT"));
+
+  // const maxFinishTimeKey = finishiTime.size
+  const sccExplored = new Set();
+  const ids = [];
+  let sccOrder = 0;
+  // const MagicOrder = R.sort((a,b)=>b-a,finishTime.keys())
+
+  // decrease Order
+  for (let i = keyStack.length - 1; i > -1; i--) {
+    let tempVertex = keyStack[i];
+    if (!sccExplored.has(tempVertex)) {
+      dfs_scc_help(originGraph.adjList, tempVertex, sccOrder);
+      sccOrder++;
+    }
+  }
+
+  function dfs_scc_help(graph, vertex, order) {
+    sccExplored.add(vertex);
+    const tempStack = [vertex];
+    if (!ids[order]) {
+      ids[order] = 1;
+    }
+    while (tempStack.length > 0) {
+      const key = tempStack.pop();
+      const verticesTodo = graph.get(key);
+      for (let i = 0; i < verticesTodo.length; i++) {
+        const item = verticesTodo[i];
+        if (!sccExplored.has(item)) {
+          sccExplored.add(item);
+          tempStack.push(item);
+          ids[order]++;
         }
-    })
-    const sortedScc = R.sort(((a,b)=>b-a), sccCnt).slice(0,10);
-    
-    console.log(sortedScc)
-    return sortedScc
+      }
+    }
+  }
+
+  const ret = ids.sort((a, b) => b - a).slice(0, 10);
+
+  console.log(ret);
+  return ret;
 }
-
-
-
-

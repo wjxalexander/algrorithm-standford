e774059d2e2ff644872873ba5d0e4c3bf6d2e6d1
wjxalexander <wjxalexander@hotmail.com>
2020-05-16 15:33:01 +0800

    heapsort

--- graphsearch/week3/heapSort.js
+++ graphsearch/week3/heapSort.js
@@ -2,9 +2,12 @@ const maxHeapMethod = require("./maxHeap")
 
 
 function heapSort(arr) {
+
     let constructMaxHeap = maxHeapMethod.buildArrToMaxHeap(arr)
+    const length = arr.length
     const ret = []
-    while (constructMaxHeap.length > 0) {
+
+    while (length > ret.length) {
         const { max, newHeap } = maxHeapMethod.extractMax(constructMaxHeap)
         constructMaxHeap = newHeap
         ret.push(max)
@@ -12,3 +15,7 @@ function heapSort(arr) {
     }
     return ret
 }
+
+const ret = heapSort([1, 4, 3, 6, 7, 8, 2, 5, 9, 12, 15, 13, 14])
+
+console.log(ret)
